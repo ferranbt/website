498b34c7a5ada9dc83df4833530ac2abf9f11369
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-02-13 21:10:54 +0000

    Add plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import Head from "next/head";
+
+const prod = process.env.NODE_ENV === "production";
 
 const navItems = [
   { href: "/", label: "Home" },
@@ -12,33 +15,44 @@ const navItems = [
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="container">
-      <header className="pt-10">
-        <div className="mx-auto flex justify-between items-center">
-          <nav>
-            <ul className="flex space-x-4">
-              {navItems.map((item) => (
-                <li key={item.href}>
-                  <Link
-                    href={item.href}
-                    className="hover:text-gray-400 underline"
-                  >
-                    {item.label}
-                  </Link>
-                  {item.external && (
-                    <ArrowTopRightOnSquareIcon className="h-4 w-4 mb-1 inline ml-1" />
-                  )}
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <main className="flex min-h-screen flex-col items-center justify-between py-14">
-        <div className="z-10 w-full items-center justify-between font-mono text-sm">
-          <Component {...pageProps} />
-        </div>
-      </main>
-    </div>
+    <>
+      <Head>
+        {prod && (
+          <script
+            defer
+            data-domain="ferranbt.com"
+            src="https://plausible.io/js/plausible.js"
+          ></script>
+        )}
+      </Head>
+      <div className="container">
+        <header className="pt-10">
+          <div className="mx-auto flex justify-between items-center">
+            <nav>
+              <ul className="flex space-x-4">
+                {navItems.map((item) => (
+                  <li key={item.href}>
+                    <Link
+                      href={item.href}
+                      className="hover:text-gray-400 underline"
+                    >
+                      {item.label}
+                    </Link>
+                    {item.external && (
+                      <ArrowTopRightOnSquareIcon className="h-4 w-4 mb-1 inline ml-1" />
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <main className="flex min-h-screen flex-col items-center justify-between py-14">
+          <div className="z-10 w-full items-center justify-between font-mono text-sm">
+            <Component {...pageProps} />
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
